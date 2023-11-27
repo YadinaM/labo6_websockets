62f6b9d4fccddda19b905eb59f8c979522c6cf06
YadinaM <yadina.moreira@gmail.com>
2023-11-27 18:54:50 +0100

    feat: add backend code for connection

---
+++ primus/live.js
@@ -0,0 +1,18 @@
+module.exports.go = (server) => {
+    const Primus = require('primus');
+    const primus = new Primus(server, {/* options */});
+
+    //check if connection
+    primus.on("connection", (spark) => {
+        console.log("connection ⭐");
+        
+        //check if data then console log
+        spark.on("data", (data) => { //spark is een individu sturen met primus is naar iedereen
+            console.log("data 🔥", data);
+
+            //send data back to all clients
+            //primus.write(data); //all
+            spark.write(data); // 1
+        })
+    })
+}
